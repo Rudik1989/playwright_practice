ca4259c11e8ea5caa6c73a6a038964a9c02f1b11
Rudik1989 <vladymyr.rudyshyn@gmail.com>
2024-01-30 23:08:41 +0200

    check job

--- tests/ui/test_main_page.py
+++ tests/ui/test_main_page.py
@@ -8,7 +8,7 @@ class TestMainPage:
     def test_empty_login_error(self, open_browser):
         PageCommon.click_on_login_button()
         PageCommon.click_on_sign_in_button()
-        assert_that(PageCommon.get_login_error_text(), equal_to('Empty login112'), "Wrong error is shown")
+        assert_that(PageCommon.get_login_error_text(), equal_to('Empty login11'), "Wrong error is shown")
 
     def test_empty_login_error1(self, open_browser):
         PageCommon.click_on_login_button()
